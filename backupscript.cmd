2295c11ff609df9dc690ca99163d458afbd67d4d
MElsberger <elsberger@it-learner.de>
2021-10-22 09:44:23 +0200

    Version1

---
+++ backupscript.cmd
@@ -0,0 +1,8 @@
+@echo off
+rem Backup des kompletten Verzeichnisses
+rem hier wird das komplette Volume g nach v kopiert
+rem inklusiven ein Protokollierung 
+
+echo "******************* Backup des Laufwerks g: nach v: **************************"
+
+"c:\Windows\system32\robocopy.exe" "g:" "v:" /Copyall /E /B /R:3 /W:10 /XD "System Volume Information" /XD "$recycle.bin" /LOG+:"c:\Logfiles\log-%date:~0,2%-%date:~3,2%-%date:~6,4%-%time:~0,2%-%time:~3,2%-%time:~6,2%.log"
